8ba70a39d0628ea3283938870b6611a4449578ec
Preeja Raveendran <preeja_raveendran@comcast.com>
2026-05-12 11:05:42 +0530

    integrate common helpers

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -33,6 +33,10 @@ git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
 
 git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
 
+cd ..
+git clone --branch feature/RDKEMW-16743 https://github.com/rdkcentral/entservices-helpers.git
+cd "$GITHUB_WORKSPACE"
+
 git clone --branch 1.0.1 https://github.com/rdkcentral/entservices-testframework.git
 
 git clone --branch main https://github.com/rdkcentral/networkmanager.git
@@ -95,7 +99,15 @@ cmake -G Ninja -S entservices-apis  -B build/entservices-apis \
 
 cmake --build build/entservices-apis --target install
 
-
+##############################
+# Build entservices-helpers
+echo "======================================================================================"
+echo "building entservices-helpers"
+cmake -G Ninja -S ../entservices-helpers -B build/entservices-helpers \
+    -DEXCEPTIONS_ENABLE=ON \
+    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake"
+cmake --build build/entservices-helpers --target install
 
 ############################
 # generating extrnal headers
