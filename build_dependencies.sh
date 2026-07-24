dfcffb40d7652a521550310f52c3400de50c4d82
rajaLucy <rajakiranyadavalli@gmail.com>
2026-07-24 12:03:16 +0530

    RDKEMW-22169:Updated testframework ref and patches to R4.4.6

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -33,7 +33,7 @@ git clone --branch R4.4.6 https://github.com/rdkcentral/Thunder.git
 
 git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
 
-git clone --branch 1.0.14 https://github.com/rdkcentral/entservices-testframework.git
+git clone --branch feature/RDKEMW-22169 https://github.com/rdkcentral/entservices-testframework.git
 
 git clone --branch v3.0.0 https://github.com/rdkcentral/networkmanager.git
 
@@ -49,7 +49,7 @@ fi
 echo "======================================================================================"
 echo "buliding thunderTools"
 cd ThunderTools
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4-Add-support-for-project-dir.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4.6-Add-support-for-project-dir.patch
 cd -
 
 
@@ -68,10 +68,9 @@ echo "==========================================================================
 echo "buliding thunder"
 
 cd Thunder
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-Add-support-for-project-dir.patch
-patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/RDKEMW-733-Add-ENTOS-IDS.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4_6.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-R4.4.6-Add-support-for-project-dir.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Jsonrpc_dynamic_error_handling_R4.4.6.patch
 cd -
 
 cmake -G Ninja -S Thunder -B build/Thunder \
