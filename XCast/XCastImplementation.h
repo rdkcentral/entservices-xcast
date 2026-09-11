b2cef2074af6cc21f91542de294b82e7c4741e5b
rkiran696 <Raja_Kiran@comcast.com>
2026-09-11 11:27:28 +0530

    Revert "Updated Xcastimplementation setpowerstate"
    This reverts commit 08ff9e878944b111d5282c0cf21b9ce76bbb7c1f.

--- XCast/XCastImplementation.h
+++ XCast/XCastImplementation.h
@@ -332,7 +332,7 @@ namespace WPEFramework
             
             void onXcastUpdatePowerStateRequest(string powerState);
             uint32_t SetNetworkStandbyMode(bool networkStandbyMode);
-            void setPowerState(const std::string& powerState);
+            bool setPowerState(const std::string& powerState);
             void updateDynamicAppCache(Exchange::IXCast::IApplicationInfoIterator* const appInfoList);
             
         public:
