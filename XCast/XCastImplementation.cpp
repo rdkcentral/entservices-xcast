b5d438dc4a4e5a84be49d9936e0794d4c67b46e3
rkiran696 <Raja_Kiran@comcast.com>
2026-09-11 10:56:07 +0530

    Reverted added logs

--- XCast/XCastImplementation.cpp
+++ XCast/XCastImplementation.cpp
@@ -1322,8 +1322,7 @@ namespace WPEFramework
                 }
                 else
                 {
-                    LOGINFO("The power state is changed [%d] -> [%d] success",cur_powerState,new_powerState);
-                    LOGINFO("Power state changed successfully");
+                    LOGINFO("changing power state [%d] -> [%d] success",cur_powerState,new_powerState);
                 }
             }
             return ret;
