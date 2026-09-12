65b0d4f5926935ea537609c25caa450b660fb09d
RajaLucy <133349260+RajaLucy@users.noreply.github.com>
2026-09-12 13:10:01 +0530

    RDKEMW-21092:Update XCastImplementation.cpp

--- XCast/XCastImplementation.cpp
+++ XCast/XCastImplementation.cpp
@@ -1319,7 +1319,7 @@ namespace WPEFramework
                 {
                     ret = false;
                     LOGINFO("Failed to change power state [%d] -> [%d] ret[%x]",cur_powerState,new_powerState,ret);
-                    LOGINFO("Set power state log added 2");
+                    LOGINFO("Log added ");
                 }
                 else
                 {
