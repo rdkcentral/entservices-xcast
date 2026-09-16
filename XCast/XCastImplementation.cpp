a260b23f9e7b4cc549c211918638aeff11ca2094
Madana Gopal <madanagopal123@gmail.com>
2026-09-16 14:50:10 +0530

    Try out visible state

--- XCast/XCastImplementation.cpp
+++ XCast/XCastImplementation.cpp
@@ -547,9 +547,11 @@ namespace WPEFramework
             {
                 case Exchange::IAppManager::APP_STATE_LOADING:
                 case Exchange::IAppManager::APP_STATE_INITIALIZING:
+                    appstate = "starting";
+                    break;
                 case Exchange::IAppManager::APP_STATE_RUNNING:
-                case Exchange::IAppManager::APP_STATE_ACTIVE:
                 case Exchange::IAppManager::APP_STATE_PAUSED:
+                case Exchange::IAppManager::APP_STATE_ACTIVE:
                     appstate = "running";
                     break;
                 case Exchange::IAppManager::APP_STATE_SUSPENDED:
