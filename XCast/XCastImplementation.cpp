31c93dd3d1a34fbb991c2b26d4eb3b063f41c348
rkiran696 <Raja_Kiran@comcast.com>
2026-09-16 09:55:56 +0530

    New Log added

--- XCast/XCastImplementation.cpp
+++ XCast/XCastImplementation.cpp
@@ -1324,7 +1324,7 @@ namespace WPEFramework
                 else
                 {
                     LOGINFO("changing power state [%d] -> [%d] success",cur_powerState,new_powerState);
-                    LOGINFO("Third log has been added");
+                    LOGINFO("Log Four has been added");
                 }
             }
             return ret;
