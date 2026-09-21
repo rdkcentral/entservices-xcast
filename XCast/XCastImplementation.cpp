627d0ddfb11951ada67c3875f45b28e9deb900a2
RajaLucy <133349260+RajaLucy@users.noreply.github.com>
2026-09-21 20:21:36 +0530

    RDKEMW-21092:Update XCastImplementation.cpp

--- XCast/XCastImplementation.cpp
+++ XCast/XCastImplementation.cpp
@@ -1327,7 +1327,7 @@ namespace WPEFramework
                 else
                 {
                     LOGINFO("changing power state [%d] -> [%d] success",cur_powerState,new_powerState);
-                    LOGINFO("Adding New log");
+                    LOGINFO("This third log adding);
                 }
             }
             return ret;
