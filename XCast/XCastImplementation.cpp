b00ab81c64d946af6914f460dad3ad4fc71a0061
RajaLucy <133349260+RajaLucy@users.noreply.github.com>
2026-09-22 15:41:15 +0530

    RDKEMW-21092:Update XCastImplementation.cpp

--- XCast/XCastImplementation.cpp
+++ XCast/XCastImplementation.cpp
@@ -337,7 +337,7 @@ namespace WPEFramework
                 checkPowerAndNetworkStandbyStates();
             }
             else {
-                LOGERR("Failed to get PowerManager instance");
+                LOGERR("Failed to get PowerManager instance - poc variant");
             }
             LOGINFO("Exiting ...");
         }
